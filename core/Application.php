a48c3b3487483ba97d63549729b3dd0d8bb09d99
ahmed ashraf <ahmashrafmans@gmail.com>
2022-04-29 20:56:53 +0200

    update

---
+++ core/Application.php
@@ -0,0 +1,12 @@
+<?php
+namespace app\core;
+class Application{
+ public static $App;
+
+public function __construct()
+{
+    $App= new Application();
+    
+}
+
+}
---
+++ public/index.php
@@ -0,0 +1,4 @@
+<?php
+
+require_once "../vendor/autoload.php";
+var_dump(app\core\Application::$App);
